35c4d1177d685b19025d5ca27ca6bd06ac8042dd
Regiane Lima <yangzhou888ac@gmail.com>
2024-01-11 17:57:54 -0300

    atualizando código

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 "use strict";
-function somar(a, b) {
+//Annotation e Inference
+function soma(a, b) {
     return a + b;
 }
 console.log(somar(10, 30));
+const produto = "Livro";
+const preco = 100;
+const carro = {
+    marca: "Audi",
+    portas: 5,
+};
+const barato = preco < 400 ? true : "produto caro";
+//Funções
+function somar(a, b) {
+    return a + b;
+}
+console.log(somar(20, 20));
+const nintendo = {
+    nome: "nintendo",
+    preco: "5000", //eu quero uma função que transforme isso em real
+};
+function transformaPreco(produto) {
+    produto.preco = 'R$' + produto.preco;
+    return produto;
+}
+const produtoNovo = transformaPreco(nintendo);
+console.log(produtoNovo);
--- script.ts
+++ script.ts
@@ -1,5 +1,38 @@
-function  somar (a: number, b: number) {
+//Annotation e Inference
+function soma(a: number, b: number) {
   return a + b;
 }
 
 console.log(somar(10, 30));
+
+const produto: string = "Livro";
+const preco: number = 100;
+const carro: {
+  marca: string;
+  portas: number;
+} = {
+  marca: "Audi",
+  portas: 5,
+};
+
+const barato = preco < 400 ? true : "produto caro";
+
+//Funções
+function somar(a: number, b: number) {
+  return a + b;
+}
+
+console.log(somar(20, 20));
+
+const nintendo = {
+  nome: "nintendo",
+  preco: "5000", //eu quero uma função que transforme isso em real
+};
+
+function transformaPreco(produto: { nome: string; preco: string }) {
+  produto.preco = 'R$' + produto.preco;
+  return produto;
+}
+
+const produtoNovo= transformaPreco(nintendo);
+console.log(produtoNovo);
