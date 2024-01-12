8a65dfd2311357255a3f1ad0569ca6116097477a
Regiane Lima <yangzhou888ac@gmail.com>
2024-01-12 16:32:32 -0300

    arquivos novos

---
+++ STRING-NUMBER-E-BOOLEAN/script.js
@@ -0,0 +1,6 @@
+"use strict";
+const frase = 'Front End';
+const valor = 500;
+const condi = preco > 100;
+console.log(typeof frase);
+console.log(typeof valor);
---
+++ STRING-NUMBER-E-BOOLEAN/script.ts
@@ -0,0 +1,6 @@
+const frase: string = 'Front End';
+const valor: number = 500;
+const condi: boolean = preco > 100;
+ 
+console.log(typeof frase);
+console.log(typeof valor);
